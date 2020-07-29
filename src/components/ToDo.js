d3385232d29dc588241167569d78b11f3313e448
Jia <jiavon@ualberta.ca>
2020-07-29 11:49:17 -0600

    functioning to do app in redux with check box to remove item

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import {removeToDo} from '../actions/todos'
+import { connect } from 'react-redux';
 
 class ToDo extends React.Component
 {
+    deleteToDo = () =>
+    {
+        const id = this.props.uniqueId;
+        this.props.dispatch( removeToDo( id ) );
+    }
     render ()
     {
+
         return (
             <li>
-                Hello! 
+                <input type="checkbox" onClick={this.deleteToDo}/>
+                {this.props.text}
             </li>
         );
     }
 }
 
-export default ToDo;
+export default  connect ()
+(ToDo);
